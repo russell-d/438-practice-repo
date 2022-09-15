48d51bdc7ea0f55f476c878936fe0ff1eb671147
russell-d <rdevera@csumb.edu>
2022-09-15 11:06:31 -0700

    lol

--- src/ApplicationMain.java
+++ src/ApplicationMain.java
@@ -2,6 +2,7 @@
 public class ApplicationMain {
 	
 	public static void main(String[] args) {
+		
 		String[] shopList = new String[] {
 				"Apples",
 				"Bananas",
@@ -13,9 +14,12 @@ public class ApplicationMain {
 				"Milk"
 		};
 		
-		for (String item: shopList) {
-			System.out.println(item);
-		}	
-       }
- 
+		System.out.println("My Shopping List");
+        System.out.println();
+        
+        for (String item: shopList) {
+        	System.out.println(item);
+        }	
+	}	
+	
 }
