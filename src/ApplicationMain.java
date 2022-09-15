6aa11dcce499c465b3b1b2f4cebd72f9a6e5ad23
russell-d <rdevera@csumb.edu>
2022-09-15 11:04:33 -0700

    add code

--- src/ApplicationMain.java
+++ src/ApplicationMain.java
@@ -1,4 +1,21 @@
 
 public class ApplicationMain {
-
+	
+	public static void main(String[] args) {
+		String[] shopList = new String[] {
+				"Apples",
+				"Bananas",
+				"Corn",
+				"Dog Food",
+				"Cereal", 
+				"Bread", 
+				"Eggs",
+				"Milk"
+		};
+		
+		for (String item: shopList) {
+			System.out.println(item);
+		}	
+       }
+ 
 }
